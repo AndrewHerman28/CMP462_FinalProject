e886260e2f19f13668ac786ca044ca0283e64657
AndrewHerman28 <148911660+AndrewHerman28@users.noreply.github.com>
2025-03-20 10:52:30 -0400

    Create Graphs.py

---
+++ Graphs.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def display_graph(self):
+  plt.plot(self.net_earnings, marker='o', linestyle='-', color='b')
+  plt.xlabel("Transactions")
+  plt.ylabel("Net Earnings")
+  plt.title("Net Earnings Over Time")
+  plt.grid()
+  plt.show()
+
+tree.display_graph()
