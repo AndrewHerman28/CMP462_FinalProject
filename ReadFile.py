6dc98425ee68f78beafeb56adfe73248303d5f4e
AndrewHerman28 <148911660+AndrewHerman28@users.noreply.github.com>
2025-04-17 09:19:33 -0400

    Update ReadFile.py

--- ReadFile.py
+++ ReadFile.py
@@ -3,18 +3,19 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 from collections import defaultdict
-import tree  # Trees.py in repo
+import Trees  # Trees.py in repo
+
 
 # GUI file picker and logic handler
-def open_file_dialog(window):
+def open_file_dialog(all_trees, root):
     file_path = filedialog.askopenfilename(
+        parent=root,
         title="Select a file",
         filetypes=[
             ("CSV files", "*.csv"),
             ("Text files", "*.txt"),
             ("All files", "*.*")
-        ],
-        parent = window
+        ]
     )
 
     if file_path:
@@ -22,13 +23,14 @@ def open_file_dialog(window):
         ext = os.path.splitext(file_path)[1].lower()
 
         if ext == '.csv' or ext == '.txt':
-            data = parse_and_build_trees(file_path)
+            data = parse_and_build_trees(file_path, all_trees)
             return data  # return for displayData
         else:
             print("Unsupported file type")
 
+
 # Function to parse and build trees from a CSV file
-def parse_and_build_trees(file_path):
+def parse_and_build_trees(file_path, all_trees):
     data = defaultdict(list)
 
     with open(file_path, 'r') as file:
@@ -42,11 +44,12 @@ def parse_and_build_trees(file_path):
             }
             data[main_category].append(subcategory_info)
 
-    lists = build_trees(data)
-    return(lists)
+    lists = build_trees(data, all_trees)
+    return (lists)  # return for displayData
+
 
 # Function to parse and build trees from a TXT file (if same structure as CSV)
-def parse_txt_and_build_trees(file_path):
+def parse_txt_and_build_trees(file_path, all_trees):
     data = defaultdict(list)
 
     with open(file_path, 'r') as file:
@@ -60,12 +63,13 @@ def parse_txt_and_build_trees(file_path):
             }
             data[main_category].append(subcategory_info)
 
-    data = build_trees(data)
-    return data
+    data = build_trees(data, all_trees)
+    return data  # return for displayData
+
 
 # Shared logic for creating and populating TempTree trees
-def build_trees(data):
-    trees = list()
+def build_trees(data, all_trees):
+    trees = set()
     # lists so can be used on view page; view page uses matplotlib which requires lists to display data
     categories = list()
     subcategories = list()
@@ -73,20 +77,20 @@ def build_trees(data):
     values = list()
 
     for category in data:
-        dataTree = tree.Tree(category)
-        if dataTree not in trees:
-            trees.append(dataTree)
         for entry in data[category]:
-            categories.append(category)
             subcategory = entry['Subcategory']
-            subcategories.append(subcategory)
             value = entry['Value']
-            values.append(value)
             date = entry['Date']
-            dates.append(date)
-            dataTree.insert(category, subcategory, date, value)
 
-    for item in trees:
-        item.display(item.root)
+            if category in all_trees is not None:
+                all_trees[category].insert(category, subcategory, date, value)
+                print(f"Added {subcategory} to {category} from existing tree")
+            else:
+                new_tree = Trees.Tree(category)
+                new_tree.insert(category, subcategory, date, value)
+                all_trees[category] = new_tree
+                print(f"Added {subcategory} to {category} creating new tree")
 
-    return categories, subcategories, dates, values, trees
+
+
+    return categories, subcategories, dates, values  # return for displayData
