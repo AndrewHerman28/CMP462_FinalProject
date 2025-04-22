80ca8c24b30bf7712bab63e4beea988290aee16e
Daisy <daisyap2005@gmail.com>
2025-04-22 13:40:56 -0400

    Update HelperFunctions.py

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -15,8 +15,8 @@ def create_label_R(self, text, color):
     label.pack(side="right", pady=10, padx=10)
     return label
 
-def create_label_B(self, text):
-    label = tk.Label(self, text=text, font=("Times New Roman", 18), bg="midnightblue")
+def create_label_B(self, text, color):
+    label = tk.Label(self, text=text, font=("Times New Roman", 18), bg=color)
     label.pack(side="bottom", pady=10)
     return label
 
@@ -25,3 +25,23 @@ def create_entry(self):
     entry.pack(side="top")
     entry.focus_set()
     return entry
+
+def prev_page(self, page_expenses):
+    if self.current_page > 0:
+        self.current_page -= 1
+        self.display_search_results(self, page_expenses)
+
+def display_search_results(self, page_expenses):
+    for item in page_expenses:
+        node = create_label_L(
+            self.searchFrame,
+            f"Expense Group: {item[0]}\n"
+            f"Expense Name: {item[1]}\n"
+            f"Expense Amount: {item[2]}\n"
+            f"Expense Date: {item[3]}",
+            "green")
+
+def next_page(self, page_expenses):
+    if (self.current_page + 1) * self.results_per_page < len(self.expenses):
+        self.current_page += 1
+        self.display_search_results(self, page_expenses)
